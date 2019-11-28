31204bc5d5d8a5a41fa618b63962888f9521cd54
page <page@beats.net.cn>
2019-11-28 11:19:46 +0800

    添加注释

--- 1/webgl.js
+++ 1/webgl.js
@@ -1,27 +1,17 @@
 //开始学习webgl，在画布上涂一些简单的颜色
 
+//获取canvas对象
 var canvas = document.getElementById("myCanvas");
+//获取WebGL对象
 var gl = canvas.getContext("experimental-webgl");
+//new一个编译对象
 var iCompile = new Compile(canvas,gl);
 
+//编译顶点着色器和片元着色器
 var vs = iCompile.create_shader("2d-vertex-shader");
 var fs = iCompile.create_shader("2d-fragment-shader");
+//创建程序对象
 var program = iCompile.create_program(vs,fs);
 
-// var buffer = gl.createBuffer();
-// gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-// gl.bufferData(
-// gl.ARRAY_BUFFER,
-// new Float32Array([
-// -1.0, -1.0,
-//  1.0, -1.0,
-// -1.0,  1.0,
-// -1.0,  1.0,
-//  1.0, -1.0,
-//  1.0,  1.0]),
-// gl.STATIC_DRAW);
-// gl.enableVertexAttribArray(positionLocation);
-// gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
- 
-// draw
+//在画布上画出我们想要的内容
 gl.drawArrays(gl.POINTS, 0, 6);   
